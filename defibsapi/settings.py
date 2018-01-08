2cafdd8a55f9d52b343f929d70aa5152c21f7604
sdob <sdob@cs.ucc.ie>
2018-01-08 10:06:09 +0000

    Fix import

--- defibsapi/settings.py
+++ defibsapi/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 from dotenv import find_dotenv, load_dotenv
-import project.get_eb_env
+import defibsapi.get_eb_env
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
