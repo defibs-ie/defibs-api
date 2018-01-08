cfb173a640e032f54a437cd5484f512c7f7077eb
sdob <sdob@cs.ucc.ie>
2018-01-08 18:50:20 +0000

    Use local .env for db settings

--- defibsapi/settings.py
+++ defibsapi/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import dj_database_url
 from dotenv import find_dotenv, load_dotenv
 import defibsapi.get_eb_env
 
@@ -85,10 +86,7 @@ WSGI_APPLICATION = 'defibsapi.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config()
 }
 
 
