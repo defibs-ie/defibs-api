f42f59e0058cb42ef408a6a9b61bd7e5b11bb366
sdob <sdob@cs.ucc.ie>
2018-01-08 18:24:47 +0000

    Add defib address field

--- defibs/models.py
+++ defibs/models.py
@@ -11,4 +11,6 @@ class Defib(models.Model):
     lat = models.DecimalField(max_digits=9, decimal_places=6)
     lon = models.DecimalField(max_digits=9, decimal_places=6)
 
-    notes = models.TextField()
+    address = models.TextField(null=True, blank=True)
+
+    notes = models.TextField(null=True, blank=True)
--- defibs/serializers.py
+++ defibs/serializers.py
@@ -5,5 +5,5 @@ from .models import Defib
 class DefibSerializer(serializers.ModelSerializer):
     class Meta:
         model = Defib
-        fields = ('id', 'lat', 'lon', 'notes',)
-        read_only_fields = ('id', 'lat', 'lon', 'notes',)
+        fields = ('id', 'lat', 'lon', 'notes', 'address',)
+        read_only_fields = ('id', 'lat', 'lon', 'notes', 'address',)
