3d083357a4d41f63d0c4ae89e9c4c46cf8bf73e2
sdob <sdob@cs.ucc.ie>
2018-01-09 13:20:40 +0000

    Get DEBUG from env

--- defibsapi/settings.py
+++ defibsapi/settings.py
@@ -31,7 +31,7 @@ load_dotenv(find_dotenv())
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['*']
 
