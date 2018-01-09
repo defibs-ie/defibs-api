a3f9f7dd34cd51b590590577dc03881549c87d42
sdob <sdob@cs.ucc.ie>
2018-01-09 16:27:40 +0000

    Register Defib in the admin

--- defibs/admin.py
+++ defibs/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Defib
+
+class DefibAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Defib, DefibAdmin)
