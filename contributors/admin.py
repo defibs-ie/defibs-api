8c4e38bea93298f13ae6c681cf418d984e5b7045
sdob <sdob@cs.ucc.ie>
2018-01-10 13:24:55 +0000

    Register Contributors on admin

--- contributors/admin.py
+++ contributors/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Contributor
+
+class ContributorAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Contributor, ContributorAdmin)
